b70169a4c499150550b9bdfacc1ea80e19603da3
Miguel Arias Moreno <arias.miguelm@gmail.com>
2023-01-10 15:52:34 -0500

    post database

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,33 +9,15 @@ use Inertia\Inertia;
 
 class PostController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    
     public function index()
     {
-        return Inertia::render('Blogs/Index',[]);
+        return Inertia::render('Blogs/Index',[
+            'posts' => Post::with('user:id,name')->latest()->get()
+        ]);
         
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         //validamos los datos
@@ -45,50 +27,22 @@ class PostController extends Controller
         ]);
 
         $request->user()->posts()->create($validated);
-        
-        return redirect(route('posts,index'));
+
+        return redirect(route('posts.index'));
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Post $post)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
-     */
     public function edit(Post $post)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
-     */
+   
     public function update(Request $request, Post $post)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
-     */
+  
     public function destroy(Post $post)
     {
         //
